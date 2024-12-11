fe50f90b12814f741945550884d4bce3033333cb
seniaz <kletsenkoov@gmail.com>
2024-12-11 20:47:00 +0200

    Deleted comment

--- Lab2/7.js
+++ Lab2/7.js
@@ -2,13 +2,6 @@
 
 'use strict';
 
-/* Do following tasks inside function `fn` (see stub: `7-objects.js`)
-- Define constant object with single field `name`.
-- Define variable object with single field `name`.
-- Try to change field `name`.
-- Try to assign other object to both identifiers.
-- Explain script behaviour. */
-
 const fn = () => {
     const obj1 = { name: 'Kletsenko' };
     let obj2 = { name: 'Kletsenko' };
